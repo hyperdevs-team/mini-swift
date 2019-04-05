ecf8520f1aaaa1e0548e1148fd9405a1f4f501a7
Sebastián Varela <sebastian.varela@masmovil.com>
2019-04-05 12:09:34 +0200

    Add support for tvOS and macOS

--- Source/Mini.h
+++ Source/Mini.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 BQ. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Mini.
 FOUNDATION_EXPORT double MiniVersionNumber;
