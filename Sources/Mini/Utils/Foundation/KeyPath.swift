210a40451dd7da2a4613ce8ae9352216c3dc4583
Jorge Revuelta Herrero <minuscorp@gmail.com>
2020-02-03 16:11:44 +0100

    KeyPath license

--- Sources/Mini/Utils/Foundation/KeyPath.swift
+++ Sources/Mini/Utils/Foundation/KeyPath.swift
@@ -1,9 +1,18 @@
-//
-//  File.swift
-//
-//
-//  Created by Jorge Revuelta on 03/02/2020.
-//
+/*
+ Copyright [2019] [BQ]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import Foundation
 
