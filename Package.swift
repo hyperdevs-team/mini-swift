775463d88466a3af74668c3ca7588fcd407f89e6
Jorge Revuelta Herrero <jorge.revuelta@bq.com>
2020-03-09 09:25:14 +0100

    Describe platform minimum requirements

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "Mini",
-    //    platforms: [
-//        .iOS(.v11),
-//        .macOS(.v10_13),
-//        .tvOS(.v11)
-//    ],
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13),
+        .tvOS(.v11),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
