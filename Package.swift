6fb983d660b87bc12061d19f9e07300d0e4f578e
Jorge Revuelta Herrero <jorge.revuelta@bq.com>
2020-03-09 09:47:27 +0100

    Unhide dependencies

--- Package.swift
+++ Package.swift
@@ -38,17 +38,17 @@ let package = Package(
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.7.1")),
         // Development
-//        .package(url: "https://github.com/Quick/Nimble", .branch("master")), // dev
-//        .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting", .branch("master")), // dev
-//        .package(url: "https://github.com/minuscorp/ModuleInterface", from: "0.0.1"), // dev
-//        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
-//        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.26.0"), // dev
-//        .package(url: "https://github.com/shibapm/Rocket", from: "0.4.0"), // dev
-//        .package(url: "https://github.com/Realm/SwiftLint", from: "0.35.0"), // dev
-//        .package(url: "https://github.com/eneko/SourceDocs", from: "0.6.1"), // dev
-//        .package(url: "https://github.com/shibapm/PackageConfig.git", from: "0.12.2"), // dev
-//        .package(url: "https://github.com/shibapm/Komondor.git", from: "1.0.0"), // dev
-//        .package(url: "https://github.com/Carthage/Commandant.git", .exact("0.17.0")), // dev
+        .package(url: "https://github.com/Quick/Nimble", .branch("master")), // dev
+        .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting", .branch("master")), // dev
+        .package(url: "https://github.com/minuscorp/ModuleInterface", from: "0.0.1"), // dev
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
+        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.26.0"), // dev
+        .package(url: "https://github.com/shibapm/Rocket", from: "0.4.0"), // dev
+        .package(url: "https://github.com/Realm/SwiftLint", from: "0.35.0"), // dev
+        .package(url: "https://github.com/eneko/SourceDocs", from: "0.6.1"), // dev
+        .package(url: "https://github.com/shibapm/PackageConfig.git", from: "0.12.2"), // dev
+        .package(url: "https://github.com/shibapm/Komondor.git", from: "1.0.0"), // dev
+        .package(url: "https://github.com/Carthage/Commandant.git", .exact("0.17.0")), // dev
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -73,7 +73,7 @@ let package = Package(
             name: "MiniPromises",
             dependencies: ["Mini"]
         ),
-//        .testTarget(name: "MiniSwiftTests", dependencies: ["Mini", "MiniTasks", "MiniPromises", "TestMiddleware", "NIOConcurrencyHelpers", "RxSwift", "Nimble", "RxTest", "RxBlocking"]), // dev
+        .testTarget(name: "MiniSwiftTests", dependencies: ["Mini", "MiniTasks", "MiniPromises", "TestMiddleware", "NIOConcurrencyHelpers", "RxSwift", "Nimble", "RxTest", "RxBlocking"]), // dev
     ],
     swiftLanguageVersions: [.version("5")]
 )
