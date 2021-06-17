af0a4a96a49c03f4dc173c6047b32e540d84470f
Alejandro Ruiz Ponce <alejandro.ruiz@techsociety.es>
2021-06-17 12:06:30 +0200

    Fix package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,10 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
+            name: "Mini",
+            targets: ["Mini"]
+        ),
+        .library(
             name: "Mini/Log",
             targets: ["LoggingService"]
         ),
@@ -26,6 +30,8 @@ let package = Package(
         ),
     ],
     dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.7.1")),
         // Development
         .package(url: "https://github.com/Quick/Nimble", .branch("main")), // dev
         .package(url: "https://github.com/mattgallagher/CwlPreconditionTesting", .branch("master")), // dev
@@ -43,6 +49,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
+            name: "Mini",
+            dependencies: ["NIOConcurrencyHelpers"]
+        ),
+        .target(
             name: "LoggingService"
         ),
         .target(
@@ -51,7 +61,7 @@ let package = Package(
         .target(
             name: "MiniTasks"
         ),
-        .testTarget(name: "MiniSwiftTests", dependencies: ["MiniTasks", "TestMiddleware", "Nimble"]), // dev
+        .testTarget(name: "MiniSwiftTests", dependencies: ["Mini", "MiniTasks", "TestMiddleware", "Nimble", "NIOConcurrencyHelpers"), // dev
     ],
     swiftLanguageVersions: [.version("5")]
 )
