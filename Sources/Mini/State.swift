5faac80fc437cb512adbc58a9347fcedb88a860c
Alejandro Ruiz Ponce <alejandro.ruiz@techsociety.es>
2021-06-30 09:38:09 +0200

    Remove function from StateType protocol

--- Sources/Mini/State.swift
+++ Sources/Mini/State.swift
@@ -4,13 +4,4 @@
  */
 import Foundation
 
-@objc public protocol StateType {
-    @objc optional func isEqual(to other: StateType) -> Bool
-}
-
-public extension StateType where Self: Equatable {
-    func isEqual(to other: StateType) -> Bool {
-        guard let other = other as? Self else { return false }
-        return self == other
-    }
-}
+public protocol StateType { }
