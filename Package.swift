f61c2b79d329f0944452ef7479c6cb66d68d95e0
Alejandro Ruiz Ponce <alejandro.ruiz@techsociety.es>
2021-11-17 14:48:07 +0100

    Fix iOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Mini",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v12),
         .macOS(.v10_13),
         .tvOS(.v11),
     ],
