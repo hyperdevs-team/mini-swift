bcdc779243763dc2760210b2c8d8b227a8cc376e
Sebastián Varela <sebastian.varela@masmovil.com>
2024-07-30 14:41:48 +0200

    Add public function to map a success task with another task publisher (#19)
    * Add public function to flatter a success task with the received value
    
    * First try
    
    * forgot project file
    
    * Fix build
    
    * Another try

---
+++ Sources/Publishers/Publishers.MapToLatestTask.swift
@@ -0,0 +1,36 @@
+import Combine
+
+public extension Publisher {
+    func mapToLatestTask<DownTask>(
+        transform: @escaping ((Output.Payload.ID) -> (AnyPublisher<DownTask, Failure>))
+    )
+    -> AnyPublisher<DownTask, Failure>
+    where Output: Taskable & Equatable, Output.Payload: Identifiable,
+          DownTask: Taskable & Equatable, DownTask.Failure == Output.Failure {
+        map { (task: Output) -> AnyPublisher<DownTask, Failure> in
+            switch task.status {
+            case .success(let payload):
+                return transform(payload.id)
+                    .removeDuplicates()
+                    .eraseToAnyPublisher()
+
+            case .idle:
+                return Just(.idle())
+                    .setFailureType(to: Failure.self)
+                    .eraseToAnyPublisher()
+
+            case .running:
+                return Just(.running())
+                    .setFailureType(to: Failure.self)
+                    .eraseToAnyPublisher()
+
+            case .failure(error: let error):
+                return Just(.failure(error))
+                    .setFailureType(to: Failure.self)
+                    .eraseToAnyPublisher()
+            }
+        }
+        .switchToLatest()
+        .eraseToAnyPublisher()
+    }
+}
